fb560a684eb84c3a294fb17508c5ad63907bd0a3
Guillaume Demerges <guillaume.demerges@protonmail.com>
2024-09-11 10:01:29 +0200

    ajout requete et connexion à la dbb

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 API_URL = "https://fakenews-eta7h8d6amhvb8hw.francecentral-01.azurewebsites.net/predict/"
+POSTGRES_CONNECTION_STRING = "dbname='fakenews' user='gdemerges' host='fakenews.postgres.database.azure.com' password='test1012@' port='5432'"
