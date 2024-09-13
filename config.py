0d2944b971e897a0e0cf7418de8260c236675778
Guillaume Demerges <guillaume.demerges@protonmail.com>
2024-09-13 08:40:42 +0200

    update config.py et ajout dans le gitignore

--- config.py
+++
@@ -1,2 +0,0 @@
-API_URL = "https://fakenews-eta7h8d6amhvb8hw.francecentral-01.azurewebsites.net/predict/"
-POSTGRES_CONNECTION_STRING = "dbname='fakenews' user='gdemerges' host= DB_HOST password= DB_PASSWORD port='5432'"
